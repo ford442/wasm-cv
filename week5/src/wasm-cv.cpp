6ae1f70d97dd00b0123a919a01bc0f26d38f5889
Noah <noahc42@gmail.com>
2022-03-27 13:58:56 -0700

    remove median close etc

--- week5/src/wasm-cv.cpp
+++ week5/src/wasm-cv.cpp
@@ -65,7 +65,6 @@ EMSCRIPTEN_KEEPALIVE void update() {
 		auto integral = makeIntegralImage(bufferPool->getCurrent(), project);
 	}
 	if (thresholdChecked) otsu(bufferPool->getCurrent(), bufferPool, project);
-	if (medianChecked) median3x3(bufferPool->getCurrent(), bufferPool, project);
 	if (dilateChecked) demoDilate5x5(bufferPool->getCurrent(), bufferPool, project);
 	if (erodeChecked) demoErode5x5(bufferPool->getCurrent(), bufferPool, project);
 	if (edgesChecked) findEdges(bufferPool->getCurrent(), bufferPool, project);
